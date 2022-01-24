37f166c08a26ce486d64f6272f1785c6ea5ceaec
loicmagne <loic.magne@outlook.com>
2022-01-24 22:58:35 +0100

    Refactored

---
+++ src/components/Charts.vue
@@ -0,0 +1,63 @@
+<script>
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
+import * as d3 from "d3"
+export default {
+    components: { 
+        Listbox, 
+        ListboxButton, 
+        ListboxOptions, 
+        ListboxOption,
+    },
+    data() {
+        return {
+            selected_reddit: 'r/CryptoCurrency',
+            selected_cc: 'BTC/USD',
+            reddits: ['r/CryptoCurrency', 'r/Bitcoin', 'r/ethereum', 'r/cardano'],
+            cc: ['BTC/USD', 'ETH/USD', 'GLM/USD', 'GRT/USD']
+        }
+  }
+}
+</script>
+
+<template>
+    <div class="flex py-2 justify-center items-center gap-4">
+        <!-- Reddit Selection -->
+        <Listbox as="div" v-model="selected_reddit">
+            <ListboxButton class="py-1 px-3 rounded-lg bg-dark-700">{{selected_reddit}}</ListboxButton>
+            <ListboxOptions class="py-1 px-3 rounded-lg bg-dark-700 absolute">
+                <ListboxOption
+                    v-for="r in reddits"
+                    :key="r"
+                    :value="r"
+                    class="cursor-pointer"
+                >
+                    {{ r }}
+                </ListboxOption>
+            </ListboxOptions>
+        </Listbox>
+
+        <h2 class="text-violet-400">vs</h2>
+        <!-- CC Selection -->
+        <Listbox as="div" v-model="selected_cc">
+            <ListboxButton class="py-1 px-3 rounded-lg bg-dark-700">{{selected_cc}}</ListboxButton>
+            <ListboxOptions class="py-1 px-3 rounded-lg bg-dark-700 absolute">
+                <ListboxOption
+                    v-for="crypto in cc"
+                    :key="crypto"
+                    :value="crypto"
+                >
+                    {{ crypto }}
+                </ListboxOption>
+            </ListboxOptions>
+        </Listbox>
+    </div>
+    <!-- Data -->
+</template>
+
+<style>
+</style>
