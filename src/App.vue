20e1700a641e59e992f2896478849e7893362499
loicmagne <loic.magne@outlook.com>
2022-01-25 02:53:31 +0100

    Added chart

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
 	<div class="text-white min-h-screen bg-dark-900 flex justify-center">
 		<div class="mt-12 flex flex-col items-center">
 			<!-- Title -->
-			<h1 class="text-6xl">Reddit <span class="text-violet-400">vs</span> CC</h1>
+			<h1 class="text-5xl">Reddit <span class="text-violet-400">vs</span> CC</h1>
 			<Charts></Charts>
 		</div>
 	</div>
--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -5,9 +5,10 @@ import {
   ListboxOptions,
   ListboxOption,
 } from '@headlessui/vue'
-import * as d3 from "d3"
+import VueApexCharts from "vue3-apexcharts";
 export default {
     components: { 
+        apexchart: VueApexCharts,
         Listbox, 
         ListboxButton, 
         ListboxOptions, 
@@ -18,18 +19,96 @@ export default {
             selected_reddit: 'r/CryptoCurrency',
             selected_cc: 'BTC/USD',
             reddits: ['r/CryptoCurrency', 'r/Bitcoin', 'r/ethereum', 'r/cardano'],
-            cc: ['BTC/USD', 'ETH/USD', 'GLM/USD', 'GRT/USD']
+            cc: ['BTC/USD', 'ETH/USD', 'GLM/USD', 'GRT/USD'],
+            data1: [30, 40, 35, 50, 49, 60, 70, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91],
+            data2: [30, 40, 35, 50, 49, 60, 70, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91, 91]
         }
-  }
+    },
+    computed: {
+        chartOptions() {
+            return {
+                chart: {
+                    id: this.selected_cc,
+                    toolbar: {
+                        show: false,
+                    },
+                },
+                stroke: {
+                    curve: 'smooth',
+                    lineCap: 'round',
+                    width: 8
+                },
+                tooltip: {
+                    enabled: true,
+                    theme: 'dark',
+                    x: {
+                        show: false
+                    }
+                },
+                xaxis: {
+                    type: 'datetime',
+                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 1999.6, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007],
+                },
+                yaxis: [{
+                    formatter: (val, index) => {
+                        return Math.round(val)
+                    },
+                    axisBorder: {
+                        show: true,
+                    },
+                    axisTicks: {
+                        show: true,
+                    },
+                    tickAmount: 10
+                },{
+                    opposite: true,
+                    formatter: (val, index) => {
+                        return Math.round(val)
+                    },
+                    axisBorder: {
+                        show: true,
+                    },
+                    axisTicks: {
+                        show: true,
+                    },
+                    tickAmount: 10
+                }],
+                grid: {
+                    show: false,
+                },
+                colors: ['#C084FC','#2DD4BF'],
+                foreColor: '#9e00e2'
+            }
+        },
+        series() {
+            return [
+                {
+                    name: this.selected_reddit,
+                    data: this.data1,
+                },
+                {
+                    name: this.selected_cc,
+                    data: this.data2,
+                },
+            ]
+        },
+    },
+    methods: {
+        resetData() {
+            this.data1 = this.data1.map(x => Math.floor(100*Math.random()))
+            this.data2 = this.data2.map(x => Math.floor(1000*Math.random()))
+        }
+    }
+
 }
 </script>
 
 <template>
-    <div class="flex py-2 justify-center items-center gap-4">
+    <div class="flex pt-2 justify-center items-center gap-4">
         <!-- Reddit Selection -->
         <Listbox as="div" v-model="selected_reddit">
             <ListboxButton class="py-1 px-3 rounded-lg bg-dark-700">{{selected_reddit}}</ListboxButton>
-            <ListboxOptions class="py-1 px-3 rounded-lg bg-dark-700 absolute">
+            <ListboxOptions class="py-1 px-3 rounded-lg bg-dark-700 absolute z-40">
                 <ListboxOption
                     v-for="r in reddits"
                     :key="r"
@@ -57,6 +136,13 @@ export default {
         </Listbox>
     </div>
     <!-- Data -->
+    <apexchart
+        width="999"
+        type="line"
+        :options="chartOptions"
+        :series="series"
+    ></apexchart>
+    <button class="bg-dark-400 rounded-lg px-2" @click="resetData"> Reset </button>
 </template>
 
 <style>
